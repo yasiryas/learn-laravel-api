1f99ef7f4d828501b36c98bcd2611b8e30f7d11f
Muhammad Yasir <yasir123983@gmail.com>
2025-12-10 21:39:40 +0700

    Feat: add seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->count(100)->create();
+        echo "100 users seeded successfully.\n";
+    }
+}
